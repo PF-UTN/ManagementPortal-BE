2900c816a5cb3eafcd9b1e652f62938cc6243dc5
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-03-02 17:47:24 -0300

    Remove any type

--- prisma/postdeploy/table-population/generate-merge-data.script.ts
+++ prisma/postdeploy/table-population/generate-merge-data.script.ts
@@ -2,24 +2,29 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const mergeTableData = async (tableName: string, values: Array<Record<string, any>>): Promise<void> => {
-  if (values.length === 0) throw new Error("Values array cannot be empty");
+export const mergeTableData = async (
+  tableName: string,
+  values: Array<Record<string, string | number | Date>>,
+): Promise<void> => {
+  if (values.length === 0) throw new Error('Values array cannot be empty');
 
   const columns = Object.keys(values[0]);
-  const idColumn = "id"; 
-  
+  const idColumn = 'id';
+
   const valuesList = values
-    .map(row => `(${columns.map(col => typeof row[col] === 'string' ? row[col].toString() : row[col]).join(", ")})`)
-    .join(",\n  ");
+    .map(
+      (row) =>
+        `(${columns.map((col) => (typeof row[col] === 'string' ? row[col].toString() : row[col])).join(', ')})`,
+    )
+    .join(',\n  ');
 
   const updateClause = columns
-    .filter(col => col !== idColumn)
-    .map(col => `${col} = EXCLUDED.${col}`)
-    .join(", ");
+    .filter((col) => col !== idColumn)
+    .map((col) => `${col} = EXCLUDED.${col}`)
+    .join(', ');
 
-  const query =  
-    `WITH inserted AS (
-      INSERT INTO "${tableName}" (${columns.join(", ")})
+  const query = `WITH inserted AS (
+      INSERT INTO "${tableName}" (${columns.join(', ')})
       VALUES
         ${valuesList}
       ON CONFLICT (${idColumn})
@@ -29,4 +34,4 @@ export const mergeTableData = async (tableName: string, values: Array<Record<str
     DELETE FROM "${tableName}" WHERE ${idColumn} NOT IN (SELECT ${idColumn} FROM inserted);`;
 
   await prisma.$executeRawUnsafe(query);
-}
+};
