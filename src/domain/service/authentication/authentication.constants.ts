21470a3b0c4984e431811d2c05f2c47832c4c39a
tomasamori <tomas.amori@gmail.com>
2025-03-03 09:19:15 -0300

    refactor: Improve constant naming and extract JWT expiration time

--- src/domain/service/authentication/authentication.constants.ts
+++ src/domain/service/authentication/authentication.constants.ts
@@ -1,3 +1,4 @@
-export const jwtConstants = {
-    secret: process.env.JWT_SECRET,
+export const JWT = {
+  SECRET: process.env.JWT_SECRET,
+  EXPIRATION: process.env.JWT_EXPIRATION,
 };
--- src/domain/service/authentication/authentication.service.module.ts
+++ src/domain/service/authentication/authentication.service.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { UserServiceModule } from '../user/user.service.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './authentication.constants';
+import { JWT } from './authentication.constants';
 
 @Module({
   imports: [UserServiceModule, JwtModule.register({
     global: true,
-    secret: jwtConstants.secret,
-    signOptions: { expiresIn: '60m' },
+    secret: JWT.SECRET,
+    signOptions: { expiresIn: JWT.EXPIRATION },
   })],
   providers: [AuthenticationService],
   exports: [AuthenticationService],
