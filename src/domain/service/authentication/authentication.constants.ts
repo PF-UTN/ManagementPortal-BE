bffa1e4af5b523e5f8a6fc98fc78e859cd273c36
tomasamori <tomas.amori@gmail.com>
2025-03-03 10:04:21 -0300

    refactor: Extract JWT configuration

--- src/domain/service/authentication/authentication.constants.ts
+++ src/domain/service/authentication/authentication.constants.ts
@@ -1,4 +1,10 @@
-export const JWT = {
-  SECRET: process.env.JWT_SECRET,
-  EXPIRATION: process.env.JWT_EXPIRATION,
-};
+import { JwtModule } from '@nestjs/jwt';
+
+const SECRET = process.env.JWT_SECRET;
+const EXPIRATION = process.env.JWT_EXPIRATION;
+
+export const JWT = JwtModule.register({
+  global: true,
+  secret: SECRET,
+  signOptions: { expiresIn: EXPIRATION },
+});
--- src/domain/service/authentication/authentication.service.module.ts
+++ src/domain/service/authentication/authentication.service.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { UserServiceModule } from '../user/user.service.module';
-import { JwtModule } from '@nestjs/jwt';
 import { JWT } from './authentication.constants';
 
 @Module({
-  imports: [UserServiceModule, JwtModule.register({
-    global: true,
-    secret: JWT.SECRET,
-    signOptions: { expiresIn: JWT.EXPIRATION },
-  })],
+  imports: [UserServiceModule, JWT],
   providers: [AuthenticationService],
   exports: [AuthenticationService],
 })
