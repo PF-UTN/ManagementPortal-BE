bfed470a6b52ec574b293911667fedc4e3e74da1
tomasamori <tomas.amori@gmail.com>
2025-03-12 18:33:08 -0300

    Add and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerConfiguration } from './configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   SwaggerConfiguration(app);
 
