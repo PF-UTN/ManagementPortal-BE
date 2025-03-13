64dead90244cff3b0ddd10e70a00c9786b23f948
tomasamori <tomas.amori@gmail.com>
2025-03-13 20:22:42 -0300

    fix: Extend SignUpCommand from Command to ensure proper return type

--- src/controllers/authentication/command/sign-up.command.ts
+++ src/controllers/authentication/command/sign-up.command.ts
@@ -1,5 +1,9 @@
 import { UserCreationDto } from '@mp/common/dtos';
+import { Command } from '@nestjs/cqrs';
+import { User } from '../../../domain/entity/user.entity';
 
-export class SignUpCommand {
-  constructor(public readonly userCreationDto: UserCreationDto) {}
+export class SignUpCommand extends Command<User> {
+  constructor(public readonly userCreationDto: UserCreationDto) {
+    super();
+  }
 }
