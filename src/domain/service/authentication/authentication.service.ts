07f39b1917a00e06904a1fcc378bd9e333aef874
tomasamori <tomas.amori@gmail.com>
2025-03-17 16:59:56 -0300

    feat: Validate hashed password in signInAsync

--- src/domain/service/authentication/authentication.service.ts
+++ src/domain/service/authentication/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthenticationService {
@@ -14,7 +15,13 @@ export class AuthenticationService {
     password: string,
   ): Promise<{ access_token: string }> {
     const user = await this.userService.findByEmailAsync(email);
-    if (user?.password !== password) {
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       throw new UnauthorizedException();
     }
 
