f47213fcba4de0ab463418ff78682d56b3aa8626
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-03-18 21:07:49 -0300

    Add DTO validators

--- libs/common/src/dtos/registration-request/search-registration-request.request.ts
+++ libs/common/src/dtos/registration-request/search-registration-request.request.ts
@@ -1,22 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { SearchRegistrationRequestFiltersDto } from './search-registration-request-filters.dto';
+import { IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class SearchRegistrationRequestRequest {
   @ApiProperty({
     example: 'search text',
     description: 'The text to search for',
+    required: false,
   })
+  @IsString()
+  @MaxLength(50)
   searchText: string;
 
-  @ApiProperty({ example: 1, description: 'The page number for pagination' })
+  @ApiProperty({
+    example: 1,
+    description: 'The page number for pagination',
+    required: true,
+  })
+  @IsNumber()
   page: number;
 
-  @ApiProperty({ example: 10, description: 'The number of items per page' })
+  @ApiProperty({
+    example: 10,
+    description: 'The number of items per page',
+    required: true,
+  })
+  @IsNumber()
   pageSize: number;
 
   @ApiProperty({
     type: SearchRegistrationRequestFiltersDto,
     description: 'The filters to apply to the search',
+    required: false,
   })
   filters: SearchRegistrationRequestFiltersDto;
 }
