81a1a6e750213198983917ab15a0a2d9d5f0b6c3
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-03-18 22:08:23 -0300

    Move exception filter configuration to correct folder

--- src/configuration/http-exception-filter.configuration.ts
+++ src/configuration/http-exception-filter.configuration.ts
@@ -1,11 +1,16 @@
-import { INestApplication } from '@nestjs/common';
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException, InternalServerErrorException } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  InternalServerErrorException,
+  INestApplication,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
-
 
 export const GlobalExceptionFilterConfiguration = (app: INestApplication) => {
   app.useGlobalFilters(new GlobalExceptionFilter());
-}
+};
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
@@ -14,16 +19,22 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
 
     const status =
-    exception instanceof HttpException
-      ? exception.getStatus()
-      : new InternalServerErrorException().getStatus();
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : new InternalServerErrorException().getStatus();
 
     response.status(status).json({
       statusCode: status,
-      message: exception instanceof HttpException ? exception.message : 'Error interno del servidor',
+      message:
+        exception instanceof HttpException
+          ? exception.message
+          : 'Error interno del servidor',
       timestamp: new Date().toISOString(),
       path: request.url,
-      stack: process.env.NODE_ENV === 'development' ? (exception as Error).stack : undefined,
+      stack:
+        process.env.NODE_ENV === 'development'
+          ? (exception as Error).stack
+          : undefined,
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerConfiguration } from './configuration';
-import { GlobalExceptionFilterConfiguration } from './http-exception.filter';
+import {
+  GlobalExceptionFilterConfiguration,
+  SwaggerConfiguration,
+} from './configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
   SwaggerConfiguration(app);
   GlobalExceptionFilterConfiguration(app);
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
 }
