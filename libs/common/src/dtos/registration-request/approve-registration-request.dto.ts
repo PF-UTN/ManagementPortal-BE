237f1e553943a7914b9589ed5c0ed6f69b8e63ff
tomasamori <tomas.amori@gmail.com>
2025-03-24 09:55:57 -0300

    feat: Add Swagger metadata to ApproveRegistrationRequestDto

--- libs/common/src/dtos/registration-request/approve-registration-request.dto.ts
+++ libs/common/src/dtos/registration-request/approve-registration-request.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, Length } from 'class-validator';
 
 export class ApproveRegistrationRequestDto {
+  @ApiProperty({
+    example: '',
+    description: 'The note to add to the registration request',
+    required: false,
+    maxLength: 50,
+  })
   @IsOptional()
   @IsString()
   @Length(0, 50)
