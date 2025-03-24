ce08bce2ba96863714e463c9c3f0f4b49fc20731
tomasamori <tomas.amori@gmail.com>
2025-03-24 09:11:26 -0300

    feat: Add MailingService for sending emails across the application

---
+++ libs/common/src/services/mailing/mailing.constants.ts
@@ -0,0 +1,8 @@
+const MAIL_HOST = process.env.MAIL_HOST;
+const MAIL_PORT = parseInt(process.env.MAIL_PORT ?? '587');
+const MAIL_SECURE = process.env.MAIL_SECURE === 'true';
+const MAIL_USER = process.env.MAIL_USER;
+const MAIL_PASS = process.env.MAIL_PASS;
+const MAIL_FROM = process.env.MAIL_FROM;
+
+export { MAIL_HOST, MAIL_PORT, MAIL_SECURE, MAIL_USER, MAIL_PASS, MAIL_FROM };
