4fa8b64de615c3297504c993c76a2c7fe69f6ff6
tomasamori <tomas.amori@gmail.com>
2025-03-27 17:23:21 -0300

    refactor SignUpCommandHandler

--- src/controllers/authentication/command/sign-up.command.handler.ts
+++ src/controllers/authentication/command/sign-up.command.handler.ts
@@ -1,15 +1,13 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { RegistrationRequestStatus } from '@mp/common/constants';
+import { RegistrationRequestStatusId } from '@mp/common/constants';
 import { SignUpCommand } from './sign-up.command';
 import { UserService } from '../../../domain/service/user/user.service';
 import { RegistrationRequestDomainService } from '../../../domain/service/registration-request/registration-request-domain.service';
-import { RegistrationRequestStatusService } from '../../../domain/service/registration-request-status/registration-request-status.service';
 
 @CommandHandler(SignUpCommand)
 export class SignUpCommandHandler implements ICommandHandler<SignUpCommand> {
   constructor(
     private readonly userService: UserService,
-    private readonly registrationRequestStatusService: RegistrationRequestStatusService,
     private readonly registrationRequestService: RegistrationRequestDomainService,
   ) {}
 
@@ -17,18 +15,10 @@ export class SignUpCommandHandler implements ICommandHandler<SignUpCommand> {
     const user = await this.userService.createUserAsync(
       command.userCreationDto,
     );
-    const status =
-      await this.registrationRequestStatusService.findByCodeAsync(RegistrationRequestStatus.Pending);
-
-    if (!status) {
-      throw new Error(
-        'RegistrationRequestStatus with code="Pending" not found',
-      );
-    }
 
     await this.registrationRequestService.createRegistrationRequestAsync({
       user: { connect: { id: user.id } },
-      status: { connect: { id: status.id } },
+      status: { connect: { id: RegistrationRequestStatusId.Pending } },
       note: '',
     });
 
