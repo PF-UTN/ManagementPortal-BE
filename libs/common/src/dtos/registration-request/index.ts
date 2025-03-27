998ec5c0e26232bde5051f3a41e677f73b8705bd
tomasamori <tomas.amori@gmail.com>
2025-03-26 22:21:04 -0300

    Create RejectRegistrationRequestDto

--- libs/common/src/dtos/registration-request/index.ts
+++ libs/common/src/dtos/registration-request/index.ts
@@ -4,4 +4,5 @@ export * from './search-registration-request-filters.dto';
 export * from './search-registration-request.request.dto';
 export * from './search-registration-request.response.dto';
 export * from './approve-registration-request.dto';
+export * from './reject-registration-request.dto';
 export * from './update-registration-request-status.dto';
