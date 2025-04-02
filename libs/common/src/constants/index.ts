bc23ffd84bbe677ff348c8a60a543e8821e78fd3
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-04-02 19:24:24 -0300

    Generate seed data

--- libs/common/src/constants/index.ts
+++ libs/common/src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './authentication.constants';
+export * from './permission-codes.constants';
 export * from './registration-request-status.constant';
 export * from './user-document-types.constant';
