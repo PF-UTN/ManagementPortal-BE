a8433c6995af65a354bb5a62d7b11243a9479be4
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-04-09 23:52:47 -0300

    Revert "Require permission on registrationRequest endpoints"
    This reverts commit 547336a900d7001756ca3d1a5179ddcdd66b8370.

--- src/controllers/registration-request/registration-request.controller.ts
+++ src/controllers/registration-request/registration-request.controller.ts
@@ -51,8 +51,6 @@ export class RegistrationRequestController {
   }
 
   @Post(':id/approve')
-  @RequiredPermissions(PermissionCodes.RegistrationRequest.UPDATE)
-  @ApiBearerAuth()
   @HttpCode(204)
   @ApiOperation({
     summary: 'Approve a registration request',
@@ -74,8 +72,6 @@ export class RegistrationRequestController {
 
   @Post(':id/reject')
   @HttpCode(204)
-  @RequiredPermissions(PermissionCodes.RegistrationRequest.UPDATE)
-  @ApiBearerAuth()
   @ApiOperation({
     summary: 'Reject a registration request',
     description: 'Reject a registration request with the provided ID.',
@@ -96,8 +92,6 @@ export class RegistrationRequestController {
 
   @Get(':id')
   @HttpCode(200)
-  @RequiredPermissions(PermissionCodes.RegistrationRequest.READ)
-  @ApiBearerAuth()
   @ApiOperation({
     summary: 'Get a registration request by ID',
     description: 'Retrieve a registration request with the provided ID.',
