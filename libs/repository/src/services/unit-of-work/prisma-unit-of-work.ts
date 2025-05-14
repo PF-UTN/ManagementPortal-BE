d4e0170f49d3efbfc19407ecd16a24a40f512994
tomasamori <tomas.amori@gmail.com>
2025-05-14 18:57:36 -0300

    feat: Implement Unit of Work pattern

---
+++ libs/repository/src/services/unit-of-work/prisma-unit-of-work.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+import { PrismaService } from '../prisma.service';
+import { IUnitOfWork } from './unit-of-work.interface';
+
+@Injectable()
+export class PrismaUnitOfWork implements IUnitOfWork {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async execute<T>(
+    work: (tx: Prisma.TransactionClient) => Promise<T>,
+  ): Promise<T> {
+    return this.prismaService.$transaction((tx) => work(tx));
+  }
+}
