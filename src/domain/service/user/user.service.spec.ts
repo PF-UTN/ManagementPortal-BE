59ed44d26e4063d1df745a882cc558aa12046019
tomasamori <tomas.amori@gmail.com>
2025-05-20 17:10:06 -0300

    test: add test to ensure BadRequestException is thrown if email already exists

--- src/domain/service/user/user.service.spec.ts
+++ src/domain/service/user/user.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Prisma } from '@prisma/client';
 import { mockDeep } from 'jest-mock-extended';
@@ -295,6 +296,18 @@ describe('UserService', () => {
         },
         txMock,
       );
+    });
+
+    it('should throw BadRequestException if email already exists', async () => {
+      // Arrange
+      jest
+        .spyOn(userRepository, 'findByEmailAsync')
+        .mockResolvedValueOnce(user);
+
+      // Act & Assert
+      await expect(
+        service.createUserWithRegistrationRequestAsync(userCreationDtoMock),
+      ).rejects.toThrow(BadRequestException);
     });
   });
 });
