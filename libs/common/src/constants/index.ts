027af427475e65cb62bd344c8b48e493825790bb
tomasamori <tomas.amori@gmail.com>
2025-05-21 20:28:58 -0300

    feat: Add TaxCategoryId enum

--- libs/common/src/constants/index.ts
+++ libs/common/src/constants/index.ts
@@ -2,3 +2,4 @@ export * from './permission-codes.constants';
 export * from './registration-request-status.constant';
 export * from './roles.constants';
 export * from './user-document-types.constant';
+export * from './tax-category.constant';
