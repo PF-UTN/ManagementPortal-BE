bc0323abf0d724c7b828c495e7247db2d31a440a
tomasamori <tomas.amori@gmail.com>
2025-06-03 17:11:47 -0300

    feat: Create ProductCreationDto and StockDto

--- libs/common/src/dtos/products/index.ts
+++ libs/common/src/dtos/products/index.ts
@@ -2,3 +2,4 @@ export * from './search-product-filters.dto';
 export * from './search-product.response.dto';
 export * from './search-product.request.dto';
 export * from './product.dto';
+export * from './product-creation.dto';
