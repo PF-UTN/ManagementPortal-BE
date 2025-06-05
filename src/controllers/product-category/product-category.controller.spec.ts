e130f4a201993cbb455148dee47e23c3e319a64c
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2025-06-05 09:25:12 -0300

    Feat: Missing Test

---
+++ src/controllers/product-category/product-category.controller.spec.ts
@@ -0,0 +1,40 @@
+import { QueryBus } from '@nestjs/cqrs';
+import { Test, TestingModule } from '@nestjs/testing';
+import { mockDeep } from 'jest-mock-extended';
+
+import { ProductCategoryController } from './product-category.controller';
+import { GetProductCategoriesQuery } from './query/get-product-categories.query';
+
+describe('ProductCategoryController', () => {
+  let controller: ProductCategoryController;
+  let queryBus: QueryBus;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductCategoryController],
+      providers: [
+
+        {
+          provide: QueryBus,
+          useValue: mockDeep<QueryBus>(),
+        },
+      ],
+    }).compile();
+
+    controller = module.get<ProductCategoryController>(ProductCategoryController);
+    queryBus = module.get<QueryBus>(QueryBus);
+});
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+  describe('getProductCategoriesAsync', () => {
+    it('should call execute on the queryBus', async () => {
+        // Act
+        const query = new GetProductCategoriesQuery();
+        await controller.getProductCategoriesAsync();
+        // Assert  
+        expect(queryBus.execute).toHaveBeenCalledWith(query);
+    });
+  });
+});
