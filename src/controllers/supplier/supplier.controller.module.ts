ea63552b80f659c98b9895b6fd33357c71119075
tomasamori <tomas.amori@gmail.com>
2025-06-11 21:58:57 -0300

    feat: Add POST /supplier endpoint for creating and updating suppliers

--- src/controllers/supplier/supplier.controller.module.ts
+++ src/controllers/supplier/supplier.controller.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { CreateSupplierCommandHandler } from './command/create-supplier.command.handler';
 import { SuppliersQueryHandler } from './query/suppliers.query.handler';
 import { SupplierController } from './supplier.controller';
 import { SupplierServiceModule } from '../../domain/service/supplier/supplier.service.module';
 
-const queryHandlers = [
-  SuppliersQueryHandler
-];
+const queryHandlers = [SuppliersQueryHandler];
+
+const commandHandlers = [CreateSupplierCommandHandler];
 
 @Module({
-  imports: [
-    SupplierServiceModule,
-  ],
+  imports: [SupplierServiceModule],
   controllers: [SupplierController],
-  providers: [...queryHandlers],
+  providers: [...queryHandlers, ...commandHandlers],
 })
 export class SupplierModule {}
