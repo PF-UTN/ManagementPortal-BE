1ce3e19f1c6e3dbe2679fff485079ff4ffaa7a7f
tomasamori <tomas.amori@gmail.com>
2025-06-17 21:18:22 -0300

    Update default birthday in Prisma migration before NOT NULL

--- prisma/migrations/20250612185959_add_birthdate_to_user/migration.sql
+++ prisma/migrations/20250612185959_add_birthdate_to_user/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "User" ADD COLUMN     "birthdate" DATE;
 
 -- Update
-UPDATE "User" SET "birthdate" = '1990-01-01' WHERE "birthdate" IS NULL;
+UPDATE "User" SET "birthdate" = '1900-01-01' WHERE "birthdate" IS NULL;
 
 -- AlterTable
 ALTER TABLE "User" ALTER COLUMN "birthdate" SET NOT NULL;
