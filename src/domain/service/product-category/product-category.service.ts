aee10842a3ec2992fcf90cb7772451a5415d46da
tomasamori <tomas.amori@gmail.com>
2025-06-19 22:34:26 -0300

    feat: Add createOrUpdateProductCategoryAsync method to ProductCategoryService

--- src/domain/service/product-category/product-category.service.ts
+++ src/domain/service/product-category/product-category.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
+import { ProductCategoryCreationDto } from '@mp/common/dtos';
 import { ProductCategoryRepository } from '@mp/repository';
 
 @Injectable()
@@ -14,5 +15,31 @@ export class ProductCategoryService {
 
   async getProductCategoriesAsync() {
     return await this.productCategoryRepository.getProductCategoriesAsync();
+  }
+
+  async createOrUpdateProductCategoryAsync(
+    productCategoryCreationData: ProductCategoryCreationDto,
+  ) {
+    const { id, ...productCategoryData } = productCategoryCreationData;
+
+    if (id) {
+      const existsProductCategory =
+        await this.productCategoryRepository.existsAsync(id);
+
+      if (!existsProductCategory) {
+        throw new BadRequestException(
+          `Product category with id ${id} does not exist.`,
+        );
+      }
+
+      return this.productCategoryRepository.updateProductCategoryAsync(
+        id,
+        productCategoryData,
+      );
+    }
+
+    return this.productCategoryRepository.createProductCategoryAsync(
+      productCategoryData,
+    );
   }
 }
