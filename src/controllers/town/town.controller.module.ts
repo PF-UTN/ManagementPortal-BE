32b3773e52bf7dcd1a56311a76709d144123ba45
tomasamori <tomas.amori@gmail.com>
2025-06-25 19:38:27 -0300

    feat: Add POST /towns/search endpoint

--- src/controllers/town/town.controller.module.ts
+++ src/controllers/town/town.controller.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { GetTownByTextQueryHandler } from './query/get-towns-by-text.query.handler';
+import { SearchTownQueryHandler } from './query/search-town-query.handler';
 import { TownController } from './town.controller';
 import { TownServiceModule } from '../../domain/service/town/town.service.module';
+
+const commandHandlers = [GetTownByTextQueryHandler, SearchTownQueryHandler];
 
 @Module({
   imports: [TownServiceModule],
   controllers: [TownController],
-  providers: [GetTownByTextQueryHandler],
+  providers: [...commandHandlers],
 })
 export class TownModule {}
--- src/controllers/town/town.controller.ts
+++ src/controllers/town/town.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 import { Public } from '@mp/common/decorators';
+import { SearchTownRequest } from '@mp/common/dtos';
 
 import { GetTownsByTextQuery } from './query/get-towns-by-text.query';
+import { SearchTownQuery } from './query/search-town-query';
 
 @Controller('towns')
 export class TownController {
@@ -20,4 +22,20 @@ export class TownController {
   searchTownsAsync(@Query('search') searchText?: string) {
     return this.queryBus.execute(new GetTownsByTextQuery(searchText || ''));
 }
+
+  @Public()
+  @HttpCode(200)
+  @Post('search')
+  @ApiOperation({
+    summary: 'Search towns with filters',
+    description:
+      'Search for towns based on the provided search text.',
+  })
+  async searchAsync(
+    @Body() searchTownRequest: SearchTownRequest,
+  ) {
+    return this.queryBus.execute(
+      new SearchTownQuery(searchTownRequest),
+    );
+  }
 }
