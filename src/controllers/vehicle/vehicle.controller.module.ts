097743b8c90842e1621f4ecd99097555e3d2726c
tomasamori <tomas.amori@gmail.com>
2025-07-09 12:02:45 -0300

    feat: Add PUT /vehicles/:id endpoint for updating vehicles

--- src/controllers/vehicle/vehicle.controller.module.ts
+++ src/controllers/vehicle/vehicle.controller.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { CreateVehicleCommandHandler } from './command/create-vehicle.command.handler';
 import { DeleteVehicleCommandHandler } from './command/delete-vehicle.command.handler';
+import { UpdateVehicleCommandHandler } from './command/update-vehicle.command.handler';
 import { SearchVehicleQueryHandler } from './query/search-vehicle-query.handler';
 import { VehicleController } from './vehicle.controller';
 import { VehicleServiceModule } from '../../domain/service/vehicle/vehicle.service.module';
@@ -10,6 +11,7 @@ const queryHandlers = [SearchVehicleQueryHandler];
 const commandHandlers = [
   CreateVehicleCommandHandler,
   DeleteVehicleCommandHandler,
+  UpdateVehicleCommandHandler,
 ];
 
 @Module({
