78bc1fd9470120cd391bcba83047a1c169d2d812
tomasamori <tomas.amori@gmail.com>
2025-07-10 20:14:18 -0300

    feat: Add RepairRepository with existsAsync and deleteRepairAsync methods

---
+++ libs/repository/src/services/repair/repair.repository.spec.ts
@@ -0,0 +1,75 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Repair } from '@prisma/client';
+import { mockDeep } from 'jest-mock-extended';
+
+import { PrismaService } from '../prisma.service';
+import { RepairRepository } from './repair.repository';
+
+describe('RepairRepository', () => {
+  let repository: RepairRepository;
+  let prismaService: PrismaService;
+  let repair: ReturnType<typeof mockDeep<Repair>>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        RepairRepository,
+        { provide: PrismaService, useValue: mockDeep(PrismaService) },
+      ],
+    }).compile();
+
+    prismaService = module.get<PrismaService>(PrismaService);
+
+    repository = module.get<RepairRepository>(RepairRepository);
+
+    repair = mockDeep<Repair>();
+
+    repair.id = 1;
+    repair.date = mockDeep<Date>();
+    repair.description = 'Puncture';
+    repair.kmPerformed = 5000;
+    repair.deleted = false;
+    repair.vehicleId = 1;
+  });
+
+  describe('existsAsync', () => {
+    it('should return true if repair exists', async () => {
+      // Arrange
+      const id = repair.id;
+      jest
+        .spyOn(prismaService.repair, 'findFirst')
+        .mockResolvedValueOnce(repair);
+
+      // Act
+      const exists = await repository.existsAsync(id);
+
+      // Assert
+      expect(exists).toBe(true);
+    });
+
+    it('should return false if repair does not exist', async () => {
+      // Arrange
+      const id = repair.id;
+      jest.spyOn(prismaService.repair, 'findFirst').mockResolvedValueOnce(null);
+
+      // Act
+      const exists = await repository.existsAsync(id);
+
+      // Assert
+      expect(exists).toBe(false);
+    });
+  });
+
+  describe('deleteRepairAsync', () => {
+    it('should update an existing repair deleted field', async () => {
+      // Arrange
+      jest.spyOn(prismaService.repair, 'update').mockResolvedValueOnce(repair);
+
+      // Act
+      const updatedRepair = await repository.deleteRepairAsync(repair.id);
+
+      // Assert
+      expect(updatedRepair).toEqual(repair);
+    });
+  });
+});
