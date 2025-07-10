182cc8335522f04057e7404c85427c2e32d9993f
tomasamori <tomas.amori@gmail.com>
2025-07-10 20:15:22 -0300

    feat: add DELETE /vehicles/repair/:id endpoint

--- src/controllers/vehicle/vehicle.controller.module.ts
+++ src/controllers/vehicle/vehicle.controller.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
 
 import { CreateVehicleCommandHandler } from './command/create-vehicle.command.handler';
+import { DeleteVehicleRepairCommandHandler } from './command/delete-vehicle-repair.command.handler';
 import { DeleteVehicleCommandHandler } from './command/delete-vehicle.command.handler';
 import { SearchVehicleQueryHandler } from './query/search-vehicle-query.handler';
 import { VehicleController } from './vehicle.controller';
+import { RepairServiceModule } from '../../domain/service/repair/repair.service.module';
 import { VehicleServiceModule } from '../../domain/service/vehicle/vehicle.service.module';
 
 const queryHandlers = [SearchVehicleQueryHandler];
 const commandHandlers = [
   CreateVehicleCommandHandler,
   DeleteVehicleCommandHandler,
+  DeleteVehicleRepairCommandHandler,
 ];
 
 @Module({
-  imports: [VehicleServiceModule],
+  imports: [VehicleServiceModule, RepairServiceModule],
   controllers: [VehicleController],
   providers: [...queryHandlers, ...commandHandlers],
 })
