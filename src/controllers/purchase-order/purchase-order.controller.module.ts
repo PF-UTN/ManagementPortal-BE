e7f1310b7b623f745cda3c86230ff8acb2e1db81
tomasamori <tomas.amori@gmail.com>
2025-07-30 22:11:25 -0300

    feat: Add GET /purchase-order/:id endpoint

--- src/controllers/purchase-order/purchase-order.controller.module.ts
+++ src/controllers/purchase-order/purchase-order.controller.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { CreatePurchaseOrderCommandHandler } from './command/create-purchase-order.command.handler';
 import { PurchaseOrderController } from './purchase-order.controller';
+import { GetPurchaseOrderByIdQueryHandler } from './query/get-purchase-order-by-id.query.handler';
 import { PurchaseOrderServiceModule } from '../../domain/service/purchase-order/purchase-order.service.module';
 
+const queryHandlers = [GetPurchaseOrderByIdQueryHandler];
 const commandHandlers = [CreatePurchaseOrderCommandHandler];
 
 @Module({
   imports: [PurchaseOrderServiceModule],
   controllers: [PurchaseOrderController],
-  providers: [...commandHandlers],
+  providers: [...queryHandlers, ...commandHandlers],
 })
 export class PurchaseOrderModule {}
--- src/controllers/purchase-order/purchase-order.controller.ts
+++ src/controllers/purchase-order/purchase-order.controller.ts
@@ -1,15 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
-import { ApiOperation, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import {
+  ApiOperation,
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger';
 
 import { PermissionCodes } from '@mp/common/constants';
 import { RequiredPermissions } from '@mp/common/decorators';
 import { PurchaseOrderCreationDto } from '@mp/common/dtos';
 
 import { CreatePurchaseOrderCommand } from './command/create-purchase-order.command';
+import { GetPurchaseOrderByIdQuery } from './query/get-purchase-order-by-id.query';
 @Controller('purchase-order')
 export class PurchaseOrderController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
 
   @Post('/')
   @RequiredPermissions(PermissionCodes.PurchaseOrder.CREATE)
@@ -25,5 +42,21 @@ export class PurchaseOrderController {
     return this.commandBus.execute(
       new CreatePurchaseOrderCommand(purchaseOrderCreationDto),
     );
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  @ApiBearerAuth()
+  @RequiredPermissions(PermissionCodes.PurchaseOrder.READ)
+  @ApiOperation({
+    summary: 'Get purchase order by ID',
+    description: 'Retrieve a purchase order with the provided ID.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'ID of the purchase order to retrieve',
+  })
+  getPurchaseOrderByIdAsync(@Param('id', ParseIntPipe) id: number) {
+    return this.queryBus.execute(new GetPurchaseOrderByIdQuery(id));
   }
 }
