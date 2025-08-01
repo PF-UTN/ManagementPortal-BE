73a0c7845adbf96336b6d51e76aed1fbe0391101
tomasamori <tomas.amori@gmail.com>
2025-07-31 21:48:44 -0300

    feat: Add DELETE /purchase-order/:id endpoint

--- src/controllers/purchase-order/purchase-order.controller.module.ts
+++ src/controllers/purchase-order/purchase-order.controller.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { CreatePurchaseOrderCommandHandler } from './command/create-purchase-order.command.handler';
+import { DeletePurchaseOrderCommandHandler } from './command/delete-purchase-order.command.handler';
 import { PurchaseOrderController } from './purchase-order.controller';
 import { GetPurchaseOrderByIdQueryHandler } from './query/get-purchase-order-by-id.query.handler';
 import { PurchaseOrderServiceModule } from '../../domain/service/purchase-order/purchase-order.service.module';
 
 const queryHandlers = [GetPurchaseOrderByIdQueryHandler];
-const commandHandlers = [CreatePurchaseOrderCommandHandler];
+const commandHandlers = [
+  CreatePurchaseOrderCommandHandler,
+  DeletePurchaseOrderCommandHandler,
+];
 
 @Module({
   imports: [PurchaseOrderServiceModule],
--- src/controllers/purchase-order/purchase-order.controller.spec.ts
+++ src/controllers/purchase-order/purchase-order.controller.spec.ts
@@ -9,6 +9,7 @@ import {
 } from '@mp/common/dtos';
 
 import { CreatePurchaseOrderCommand } from './command/create-purchase-order.command';
+import { DeletePurchaseOrderCommand } from './command/delete-purchase-order.command';
 import { PurchaseOrderController } from './purchase-order.controller';
 import { GetPurchaseOrderByIdQuery } from './query/get-purchase-order-by-id.query';
 
@@ -89,7 +90,7 @@ describe('PurchaseOrderController', () => {
           {
             id: 1,
             productId: 1,
-            productName: 'Test Product',
+            productName: 'Test PurchaseOrder',
             unitPrice: 10.0,
             quantity: 10,
             subtotalPrice: 100.0,
@@ -108,6 +109,20 @@ describe('PurchaseOrderController', () => {
 
       // Assert
       expect(executeSpy).toHaveBeenCalledWith(expectedQuery);
+    });
+  });
+
+  describe('deletePurchaseOrderAsync', () => {
+    it('should call execute on the commandBus with correct parameters', async () => {
+      // Arrange
+      const executeSpy = jest.spyOn(commandBus, 'execute');
+      const expectedCommand = new DeletePurchaseOrderCommand(1);
+
+      // Act
+      await controller.deletePurchaseOrderAsync(1);
+
+      // Assert
+      expect(executeSpy).toHaveBeenCalledWith(expectedCommand);
     });
   });
 });
