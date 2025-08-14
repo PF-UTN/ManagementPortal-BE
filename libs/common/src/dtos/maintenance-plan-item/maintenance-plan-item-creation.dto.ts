2c86a42c21c7e303306c68cdcbfdb02f1dcbacfa
tomasamori <tomas.amori@gmail.com>
2025-08-14 18:16:16 -0300

    fix: Validate kmInterval and timeInterval as positive values in MaintenancePlanItemCreationDto

--- libs/common/src/dtos/maintenance-plan-item/maintenance-plan-item-creation.dto.ts
+++ libs/common/src/dtos/maintenance-plan-item/maintenance-plan-item-creation.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, Min } from 'class-validator';
 
 export class MaintenancePlanItemCreationDto {
   @ApiProperty({ example: 1, required: true })
@@ -15,10 +15,12 @@ export class MaintenancePlanItemCreationDto {
   @ApiProperty({ example: 10000, required: false })
   @IsNumber()
   @IsOptional()
+  @Min(0)
   kmInterval: number | null;
 
   @ApiProperty({ example: 6, required: false })
   @IsNumber()
   @IsOptional()
+  @Min(0)
   timeInterval: number | null;
 }
