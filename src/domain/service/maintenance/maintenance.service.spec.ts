0701e77ed179b45968d54a5915a4211819b979a9
tomasamori <tomas.amori@gmail.com>
2025-08-19 19:30:55 -0300

    feat: Add searchByTextAndVehicleIdAsync method in MaintenanceService

---
+++ src/domain/service/maintenance/maintenance.service.spec.ts
@@ -0,0 +1,97 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { mockDeep } from 'jest-mock-extended';
+
+import {
+  MaintenanceRepository,
+  ServiceSupplierRepository,
+  VehicleRepository,
+} from '@mp/repository';
+
+import { MaintenanceService } from './maintenance.service';
+import { SearchMaintenanceQuery } from '../../../controllers/vehicle/query/search-maintenance-query';
+
+describe('MaintenanceService', () => {
+  let service: MaintenanceService;
+  let repository: MaintenanceRepository;
+  let vehicleRepository: VehicleRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        MaintenanceService,
+        {
+          provide: MaintenanceRepository,
+          useValue: mockDeep(MaintenanceRepository),
+        },
+        {
+          provide: VehicleRepository,
+          useValue: mockDeep(VehicleRepository),
+        },
+        {
+          provide: ServiceSupplierRepository,
+          useValue: mockDeep(ServiceSupplierRepository),
+        },
+      ],
+    }).compile();
+
+    repository = module.get<MaintenanceRepository>(MaintenanceRepository);
+    vehicleRepository = module.get<VehicleRepository>(VehicleRepository);
+
+    service = module.get<MaintenanceService>(MaintenanceService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('searchByTextAndVehicleIdAsync', () => {
+    it('should throw NotFoundException if vehicle does not exist', async () => {
+      // Arrange
+      const searchText = 'test';
+      const page = 1;
+      const pageSize = 10;
+      const vehicleId = 1;
+
+      const query = new SearchMaintenanceQuery(vehicleId, {
+        searchText,
+        page,
+        pageSize,
+      });
+
+      jest.spyOn(vehicleRepository, 'existsAsync').mockResolvedValue(false);
+
+      // Act & Assert
+      await expect(
+        service.searchByTextAndVehicleIdAsync(query),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should call searchByTextAndVehicleIdAsync on the repository with correct parameters', async () => {
+      // Arrange
+      const searchText = 'test';
+      const page = 1;
+      const pageSize = 10;
+      const vehicleId = 1;
+
+      const query = new SearchMaintenanceQuery(vehicleId, {
+        searchText,
+        page,
+        pageSize,
+      });
+
+      jest.spyOn(vehicleRepository, 'existsAsync').mockResolvedValue(true);
+
+      // Act
+      await service.searchByTextAndVehicleIdAsync(query);
+
+      // Assert
+      expect(repository.searchByTextAndVehicleIdAsync).toHaveBeenCalledWith(
+        query.searchText,
+        query.page,
+        query.pageSize,
+        query.vehicleId,
+      );
+    });
+  });
+});
---
+++ src/domain/service/maintenance/maintenance.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { MaintenanceRepository, VehicleRepository } from '@mp/repository';
+
+import { SearchMaintenanceQuery } from '../../../controllers/vehicle/query/search-maintenance-query';
+
+@Injectable()
+export class MaintenanceService {
+  constructor(
+    private readonly maintenanceRepository: MaintenanceRepository,
+    private readonly vehicleRepository: VehicleRepository,
+  ) {}
+
+  async searchByTextAndVehicleIdAsync(query: SearchMaintenanceQuery) {
+    const existsVehicle = await this.vehicleRepository.existsAsync(
+      query.vehicleId,
+    );
+
+    if (!existsVehicle) {
+      throw new NotFoundException(
+        `Vehicle with id ${query.vehicleId} does not exist.`,
+      );
+    }
+
+    return await this.maintenanceRepository.searchByTextAndVehicleIdAsync(
+      query.searchText,
+      query.page,
+      query.pageSize,
+      query.vehicleId,
+    );
+  }
+}
