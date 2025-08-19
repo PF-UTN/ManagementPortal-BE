6729e0424f4d25f49ad5d8fe916fe0aead305130
tomasamori <tomas.amori@gmail.com>
2025-08-19 17:45:38 -0300

    feat: Add RepairSummaryDto, SearchRepairRequest and SearchRepairResponse

--- libs/common/src/dtos/repair/index.ts
+++ libs/common/src/dtos/repair/index.ts
@@ -1,3 +1,6 @@
 export * from './repair-creation-data.dto';
 export * from './repair-creation.dto';
 export * from './repair.dto';
+export * from './search-repair.request.dto';
+export * from './search-repair.response.dto';
+export * from './repair-summary.dto';
---
+++ libs/common/src/dtos/repair/search-repair.request.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDefined, IsNumber, IsString, MaxLength } from 'class-validator';
+
+export class SearchRepairRequest {
+  @ApiProperty({
+    example: 'Filter change',
+    description: 'The text to search for',
+    required: true,
+  })
+  @IsDefined()
+  @IsString()
+  @MaxLength(50)
+  searchText: string;
+
+  @ApiProperty({
+    example: 1,
+    description: 'The page number for pagination',
+    required: false,
+  })
+  @IsNumber()
+  page: number;
+
+  @ApiProperty({
+    example: 10,
+    description: 'The number of items per page',
+    required: false,
+  })
+  @IsNumber()
+  pageSize: number;
+}
