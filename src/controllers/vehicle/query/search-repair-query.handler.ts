88df5c7c67b563291def3da2ce00d0da01e1f4c7
tomasamori <tomas.amori@gmail.com>
2025-08-19 17:46:38 -0300

    feat: Create SearchRepairQuery and handler

---
+++ src/controllers/vehicle/query/search-repair-query.handler.ts
@@ -0,0 +1,32 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+import { SearchRepairResponse, RepairSummaryDto } from '@mp/common/dtos';
+
+import { SearchRepairQuery } from './search-repair-query';
+import { RepairService } from '../../../domain/service/repair/repair.service';
+
+@QueryHandler(SearchRepairQuery)
+export class SearchRepairQueryHandler
+  implements IQueryHandler<SearchRepairQuery>
+{
+  constructor(private readonly repairService: RepairService) {}
+
+  async execute(query: SearchRepairQuery): Promise<SearchRepairResponse> {
+    const { data, total } =
+      await this.repairService.searchByTextAndVehicleIdAsync(query);
+
+    const mappedResponse = data.map((repair): RepairSummaryDto => {
+      return {
+        id: repair.id,
+        date: repair.date,
+        description: repair.description,
+        kmPerformed: repair.kmPerformed,
+      };
+    });
+
+    return new SearchRepairResponse({
+      total,
+      results: mappedResponse,
+    });
+  }
+}
