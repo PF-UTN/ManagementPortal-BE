96270e2831bccda6ec3e95aea8d89cd99b80edf7
tomasamori <tomas.amori@gmail.com>
2025-08-26 19:59:06 -0300

    feat: Create UpdateRepairDto

--- libs/common/src/dtos/repair/index.ts
+++ libs/common/src/dtos/repair/index.ts
@@ -4,3 +4,4 @@ export * from './repair.dto';
 export * from './search-repair.request.dto';
 export * from './search-repair.response.dto';
 export * from './repair-summary.dto';
+export * from './update-repair.dto';
