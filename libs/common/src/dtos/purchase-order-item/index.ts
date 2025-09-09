f1cba1a01d2424781efc7981f98c25f6f672d6ed
tomasamori <tomas.amori@gmail.com>
2025-09-08 21:01:08 -0300

    feat: Create PurchaseOrderReportGenerationData and PurchaseOrderItemReportGenerationData DTOs

--- libs/common/src/dtos/purchase-order-item/index.ts
+++ libs/common/src/dtos/purchase-order-item/index.ts
@@ -1,3 +1,4 @@
 export * from './purchase-order-item.dto';
 export * from './purchase-order-item-details.dto';
 export * from './purchase-order-item-creation.dto';
+export * from './purchase-order-item-report-generation-data.dto';
---
+++ libs/common/src/dtos/purchase-order-item/purchase-order-item-report-generation-data.dto.ts
@@ -0,0 +1,6 @@
+export class PurchaseOrderItemReportGenerationDataDto {
+  productName: string;
+  quantity: number;
+  unitPrice: number;
+  subtotalPrice: number;
+}
