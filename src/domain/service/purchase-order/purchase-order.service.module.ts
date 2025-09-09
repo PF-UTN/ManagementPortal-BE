390e071508b10914b7c6e5e9e87d9487af838387
tomasamori <tomas.amori@gmail.com>
2025-09-08 21:04:02 -0300

    feat: Add sendPurchaseOrderByEmailAsync and invoke it on purchase order create/update

--- src/domain/service/purchase-order/purchase-order.service.module.ts
+++ src/domain/service/purchase-order/purchase-order.service.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { MailingServiceModule, ReportServiceModule } from '@mp/common/services';
 import { RepositoryModule } from '@mp/repository';
 
 import { PurchaseOrderService } from './purchase-order.service';
 import { StockServiceModule } from '../stock/stock.service.module';
 
 @Module({
-  imports: [RepositoryModule, StockServiceModule],
+  imports: [
+    RepositoryModule,
+    StockServiceModule,
+    MailingServiceModule,
+    ReportServiceModule,
+  ],
   providers: [PurchaseOrderService],
   exports: [PurchaseOrderService],
 })
