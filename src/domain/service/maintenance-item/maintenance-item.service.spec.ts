bb5cf32b29d3d0331d77917734da2770e6a63b8a
tomasamori <tomas.amori@gmail.com>
2025-09-11 21:56:39 -0300

    feat: Add searchByTextAsync and updateMaintenanceItemAsync methods in service

--- src/domain/service/maintenance-item/maintenance-item.service.spec.ts
+++ src/domain/service/maintenance-item/maintenance-item.service.spec.ts
@@ -1,11 +1,16 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MaintenanceItem } from '@prisma/client';
 import { mockDeep } from 'jest-mock-extended';
 
-import { MaintenanceItemCreationDto } from '@mp/common/dtos';
+import {
+  MaintenanceItemCreationDto,
+  UpdateMaintenanceItemDto,
+} from '@mp/common/dtos';
 import { MaintenanceItemRepository } from '@mp/repository';
 
 import { MaintenanceItemService } from './maintenance-item.service';
+import { SearchMaintenanceItemQuery } from '../../../controllers/vehicle/query/search-maintenance-item-query';
 
 describe('MaintenanceItemService', () => {
   let service: MaintenanceItemService;
@@ -57,6 +62,74 @@ describe('MaintenanceItemService', () => {
       expect(repository.createMaintenanceItemAsync).toHaveBeenCalledWith(
         maintenanceItemCreationDtoMock,
       );
+    });
+  });
+
+  describe('searchByTextAsync', () => {
+    it('should call searchByTextAsync on the repository with correct parameters', async () => {
+      // Arrange
+      const searchText = 'test';
+      const page = 1;
+      const pageSize = 10;
+      const query = new SearchMaintenanceItemQuery({
+        searchText,
+        page,
+        pageSize,
+      });
+
+      // Act
+      await service.searchByTextAsync(query);
+
+      // Assert
+      expect(repository.searchByTextAsync).toHaveBeenCalledWith(
+        query.searchText,
+        query.page,
+        query.pageSize,
+      );
+    });
+  });
+
+  describe('updateMaintenanceItemAsync', () => {
+    let maintenanceItemUpdateDtoMock: UpdateMaintenanceItemDto;
+
+    beforeEach(() => {
+      maintenanceItemUpdateDtoMock = {
+        description: maintenanceItem.description,
+      };
+    });
+
+    it('should call maintenanceItemRepository.updateMaintenanceItemAsync with correct data', async () => {
+      // Arrange
+      jest.spyOn(repository, 'existsAsync').mockResolvedValueOnce(true);
+
+      const updateMaintenanceItemAsyncSpy = jest
+        .spyOn(repository, 'updateMaintenanceItemAsync')
+        .mockResolvedValueOnce(maintenanceItem);
+
+      // Act
+      await service.updateMaintenanceItemAsync(
+        maintenanceItem.id,
+        maintenanceItemUpdateDtoMock,
+      );
+
+      // Assert
+      expect(updateMaintenanceItemAsyncSpy).toHaveBeenCalledWith(
+        maintenanceItem.id,
+        maintenanceItemUpdateDtoMock,
+      );
+    });
+
+    it('should throw NotFoundException if maintenanceItem does not exist', async () => {
+      // Arrange
+      jest.spyOn(repository, 'existsAsync').mockResolvedValueOnce(false);
+
+      // Act & Assert
+      await expect(
+        service.updateMaintenanceItemAsync(
+          maintenanceItem.id,
+          maintenanceItemUpdateDtoMock,
+        ),
+      ).rejects.toThrow(NotFoundException);
     });
   });
 });
--- src/domain/service/maintenance-item/maintenance-item.service.ts
+++ src/domain/service/maintenance-item/maintenance-item.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
-import { MaintenanceItemCreationDto } from '@mp/common/dtos';
+import {
+  MaintenanceItemCreationDto,
+  UpdateMaintenanceItemDto,
+} from '@mp/common/dtos';
 import { MaintenanceItemRepository } from '@mp/repository';
+
+import { SearchMaintenanceItemQuery } from '../../../controllers/vehicle/query/search-maintenance-item-query';
 
 @Injectable()
 export class MaintenanceItemService {
@@ -14,6 +19,33 @@ export class MaintenanceItemService {
   ) {
     return await this.maintenanceItemRepository.createMaintenanceItemAsync(
       maintenanceItemCreationDto,
+    );
+  }
+
+  async searchByTextAsync(query: SearchMaintenanceItemQuery) {
+    return await this.maintenanceItemRepository.searchByTextAsync(
+      query.searchText,
+      query.page,
+      query.pageSize,
+    );
+  }
+
+  async updateMaintenanceItemAsync(
+    id: number,
+    updateMaintenanceItemDto: UpdateMaintenanceItemDto,
+  ) {
+    const existsMaintenanceItem =
+      await this.maintenanceItemRepository.existsAsync(id);
+
+    if (!existsMaintenanceItem) {
+      throw new NotFoundException(
+        `Maintenance item with id ${id} does not exist.`,
+      );
+    }
+
+    return await this.maintenanceItemRepository.updateMaintenanceItemAsync(
+      id,
+      updateMaintenanceItemDto,
     );
   }
 }
