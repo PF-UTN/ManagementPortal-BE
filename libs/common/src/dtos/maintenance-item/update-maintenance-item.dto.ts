b86e9cc2cbec8d29caa0c0881206a1ad0d613948
tomasamori <tomas.amori@gmail.com>
2025-09-15 17:48:25 -0300

    fix: Do not allow empty description

--- libs/common/src/dtos/maintenance-item/update-maintenance-item.dto.ts
+++ libs/common/src/dtos/maintenance-item/update-maintenance-item.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDefined, IsString, MaxLength } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class UpdateMaintenanceItemDto {
   @ApiProperty({
@@ -9,6 +9,7 @@ export class UpdateMaintenanceItemDto {
   })
   @IsDefined()
   @IsString()
+  @IsNotEmpty()
   @MaxLength(500)
   description: string;
 }
