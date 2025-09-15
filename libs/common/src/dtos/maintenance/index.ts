82afa7469c9c7cb85c229aa893c896026c552ba9
tomasamori <tomas.amori@gmail.com>
2025-09-15 16:42:27 -0300

    feat: Create UpdateMaintenanceDto

--- libs/common/src/dtos/maintenance/index.ts
+++ libs/common/src/dtos/maintenance/index.ts
@@ -2,3 +2,4 @@ export * from './search-maintenance.request.dto';
 export * from './search-maintenance.response.dto';
 export * from './maintenance-summary.dto';
 export * from './maintenance-creation.dto';
+export * from './update-maintenance.dto';
