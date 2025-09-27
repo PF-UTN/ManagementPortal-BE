a83810e8f0c00e71f4437540b3b4efd8df4b28f3
tomasamori <tomas.amori@gmail.com>
2025-09-27 11:55:21 -0300

    feat: Create ShipmentRepository with createShipmentAsync method

---
+++ libs/repository/src/services/shipment/shipment.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Shipment } from '@prisma/client';
+
+import { ShipmentCreationDataDto } from '@mp/common/dtos';
+
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class ShipmentRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createShipmentAsync(
+    data: ShipmentCreationDataDto,
+    tx?: Prisma.TransactionClient,
+  ): Promise<Shipment> {
+    const { vehicleId, statusId, orderIds, ...shipmentData } = data;
+    const client = tx ?? this.prisma;
+    return client.shipment.create({
+      data: {
+        ...shipmentData,
+        vehicle: {
+          connect: { id: vehicleId },
+        },
+        status: {
+          connect: { id: statusId },
+        },
+        orders: {
+          connect: orderIds.map((id) => ({ id })),
+        },
+      },
+    });
+  }
+}
