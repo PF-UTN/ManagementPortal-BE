164255db27cbe6f5db616247dc37e91124f3b849
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-09-30 01:07:47 -0300

    Add common dto and constants

--- libs/common/src/constants/delivery-methods.constants.ts
+++ libs/common/src/constants/delivery-methods.constants.ts
@@ -2,3 +2,8 @@ export enum DeliveryMethodId {
   PickUpAtStore = 1,
   HomeDelivery = 2,
 }
+
+export const DeliveryMethodNameToIdMap: Record<string, number> = {
+  PickUpAtStore: DeliveryMethodId.PickUpAtStore,
+  HomeDelivery: DeliveryMethodId.HomeDelivery,
+};
