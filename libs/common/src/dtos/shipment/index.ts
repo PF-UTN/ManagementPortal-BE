eefe94f72b0b3879338569e421846cf52f10b1a3
tomasamori <tomas.amori@gmail.com>
2025-10-06 19:21:36 -0300

    feat: Create GetShipmentByIdDto

--- libs/common/src/dtos/shipment/index.ts
+++ libs/common/src/dtos/shipment/index.ts
@@ -2,3 +2,4 @@ export * from './shipment-creation.dto';
 export * from './shipment-creation-data.dto';
 export * from './finish-shipment-orders.dto';
 export * from './finish-shipment.dto';
+export * from './get-shipment-by-id.dto';
