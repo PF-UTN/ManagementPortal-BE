9d97cf81593410fbe9058e4d56bcccdd1cfbdb84
tomasamori <tomas.amori@gmail.com>
2025-10-06 21:33:30 -0300

    feat: Create SearchShipmentQuery and its handler

---
+++ src/controllers/shipment/query/search-shipment.query.handler.ts
@@ -0,0 +1,45 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+import { shipmentStatusTranslations } from '@mp/common/constants';
+import {
+  SearchShipmentReturnDataDto,
+  SearchShipmentResponse,
+} from '@mp/common/dtos';
+
+import { SearchShipmentQuery } from './search-shipment.query';
+import { ShipmentService } from '../../../domain/service/shipment/shipment.service';
+
+@QueryHandler(SearchShipmentQuery)
+export class SearchShipmentQueryHandler
+  implements IQueryHandler<SearchShipmentQuery>
+{
+  constructor(private readonly shipmentService: ShipmentService) {}
+
+  async execute(query: SearchShipmentQuery): Promise<SearchShipmentResponse> {
+    const { data, total } =
+      await this.shipmentService.searchWithFiltersAsync(query);
+
+    const mappedResponse = data.map((shipment): SearchShipmentReturnDataDto => {
+      return {
+        id: shipment.id,
+        date: shipment.date,
+        vehicle: {
+          id: shipment.vehicle.id,
+          licensePlate: shipment.vehicle.licensePlate,
+          brand: shipment.vehicle.brand,
+          model: shipment.vehicle.model,
+        },
+        status: shipmentStatusTranslations[shipment.status.name],
+        orders: shipment.orders.map((order) => order.id),
+        estimatedKm: shipment.estimatedKm ? Number(shipment.estimatedKm) : null,
+        effectiveKm: shipment.effectiveKm ? Number(shipment.effectiveKm) : null,
+        routeLink: shipment.routeLink,
+      };
+    });
+
+    return new SearchShipmentResponse({
+      total,
+      results: mappedResponse,
+    });
+  }
+}
